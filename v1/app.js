d68ea887f23e2f8adb7bcf7fbde3b5cd15a22cd9
Miran Ahmed <123806039+miran5882@users.noreply.github.com>
2024-09-23 02:06:23 +0530

    Update app.js

--- v1/app.js
+++ v1/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Version 1!');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(port, () => {
   console.log(`App version 1 listening at http://localhost:${port}`);
 });
